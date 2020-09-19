994d2363236f130aa58a2bf49094ec94ed77d03a
Nikita <nikitalalwani93@gmail.com>
2020-09-19 11:21:39 -0700

    Initial Commit

---
+++ EarthquakeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EarthquakeApp
+//
+//  Created by nikita lalwani on 9/19/20.
+//  Copyright © 2020 nikita lalwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
